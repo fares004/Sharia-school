a3d5cfdd8e6ca424fba68f0c21caa5d550054cd8
Fares Moaz Fares <fares.sy.30050@gmail.com>
2024-08-31 01:47:36 +0300

    Adjust the timer

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
           
           setTimeout(() => {
               iconXmark.classList.remove('rotate-in');
-          }, 2000);
+          }, 500);
       } else {
           iconXmark.classList.add('rotate-in');
           
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
               iconBars.style.display = 'block';
               iconXmark.classList.remove('rotate-in');
               navbar.style.display = 'none'; // إخفاء navbar عند إغلاق القائمة
-          }, 2000);
+          }, 500);
       }
   });
 });
