302c0aa83c00e7cb774dd2a96a81aeb0e5a95cb2
Fares Moaz Fares <fares.sy.30050@gmail.com>
2024-08-30 20:36:11 +0300

    Educational school title

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// الحصول على العناصر من DOM
-const toggleMenu = document.querySelector('.toggle-menu');
-const navbar = document.querySelector('.navbar');
-const iconBars = document.querySelector('.fa-bars');
-const iconXmark = document.querySelector('.fa-xmark');
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.add('active'); // اجعل navbar مفتوحًا عند تحميل الصفحة
 
-// إخفاء رمز الإغلاق في البداية
-iconXmark.style.display = 'none';
+  const toggleMenu = document.querySelector('.toggle-menu');
+  const iconBars = document.querySelector('.fa-bars');
+  const iconXmark = document.querySelector('.fa-xmark');
 
-// إضافة حدث النقر
-toggleMenu.addEventListener('click', function() {
-  // تبديل الفئة 'active' على عنصر الـ navbar
-  navbar.classList.toggle('active');
-  
-  if (navbar.classList.contains('active')) {
-    // إظهار أيقونة xmark مع أنيميشن
-    iconXmark.classList.add('rotate-in');
-    iconBars.style.display = 'none';
-    iconXmark.style.display = 'block';
+  // إخفاء رمز الإغلاق في البداية
+  iconXmark.style.display = 'none';
+
+  // إضافة حدث النقر
+  toggleMenu.addEventListener('click', function() {
+    navbar.classList.toggle('active');
     
-    setTimeout(() => {
-      iconXmark.classList.remove('rotate-in');
-    }, 500); // مدة الأنيميشن 0.5 ثانية
-  } else {
-    // إخفاء أيقونة xmark
-    iconXmark.classList.add('rotate-in');
-    
-    setTimeout(() => {
-      iconXmark.style.display = 'none';
-      iconBars.style.display = 'block';
-      iconXmark.classList.remove('rotate-in');
-    }, 500); // تأخير لمدة 0.5 ثانية قبل إظهار أيقونة bars
-  }
+    if (navbar.classList.contains('active')) {
+      iconXmark.classList.add('rotate-in');
+      iconBars.style.display = 'none';
+      iconXmark.style.display = 'block';
+      
+      setTimeout(() => {
+        iconXmark.classList.remove('rotate-in');
+      }, 500);
+    } else {
+      iconXmark.classList.add('rotate-in');
+      
+      setTimeout(() => {
+        iconXmark.style.display = 'none';
+        iconBars.style.display = 'block';
+        iconXmark.classList.remove('rotate-in');
+      }, 500);
+    }
+  });
 });
